a36a6a737483e9039142e773020bababfd8a6002
Raj Ashish Desai <rajdesai94@gmail.com>
2018-05-30 23:28:00 -0400

    remove comments

--- src/components/Events.js
+++ src/components/Events.js
@@ -5,7 +5,7 @@ const getEventTile = (eventName, date, posterUrl) => (
   <div className="col-md-6 col-xs-12 text-center">
     <div className="date">
       <span className="name">{eventName}</span> - {date}
-            </div>
+    </div>
     <div className="text-center">
       <img src={posterUrl} alt="Sangam_postcard_5_x_7_1" className="poster img-rounded" />
     </div>
@@ -28,47 +28,6 @@ export default function Events() {
               <img src="https://image.ibb.co/fPdUtJ/0.jpg" alt="bhairavi_Logo" className="main img-rounded" />
             </div>
           </div>
-          {/* <div className="col-md-8 col-md-offset-2 col-xs-12">
-            <h2>Brought to you by:</h2>
-          </div>
-          <div className="col-md-8 col-md-offset-2 col-xs-12 text-center sponsors">
-            <div className="row">
-              <div className="col-xs-10 col-xs-offset-1 text-center">
-                <div className="sponsor">
-                  <img src="https://image.ibb.co/ituAy5/ShopLogo.jpg" className="img-rounded" alt="sponsor-logo"/>
-                  <hr className="yellow" />
-                </div>
-                <div className="sponsor">
-                  <h4>Rahul & Meena Shukla from SS White Aerospace</h4>
-                  <hr className="orange" />
-                </div>
-                <div className="sponsor">
-                  <img src="https://image.ibb.co/cvQYJ5/Screen_Shot_2017_04_26_at_10_31_42_PM.png" alt="Screen_Shot_2017_04_26_at_10_31_42_PM" className="img-rounded" />
-                  <hr className="yellow" />
-                </div>
-                <div className="sponsor">
-                  <h4>Parikh Network</h4>
-                  <hr className="orange" />
-                </div>
-                <div className="sponsor">
-                  <img src="https://image.ibb.co/cpWMak/ED63_ADDB_0772_46_A6_8411_43_F421753845.jpg" alt="ED63_ADDB_0772_46_A6_8411_43_F421753845" />
-                  <hr className="yellow" />
-                </div>
-                <div className="sponsor">
-                  <h4>Sangita & Pranav Shah</h4>
-                  <hr className="orange" />
-                </div>
-                <div className="sponsor">
-                  <h4>Division 10 Inc</h4>
-                  <hr className="yellow" />
-                </div>
-                <div className="sponsor">
-                  <h4>Maa Siddheshwari Food Corp</h4>
-                  <hr className="orange" />
-                </div>
-              </div>
-            </div>
-          </div> */}
         </div>
       </div>
 
