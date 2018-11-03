2d314fb554f4bd3b97b7bcb57731004a7fb279c1
Raj Ashish Desai <rajdesai94@gmail.com>
2018-11-03 00:49:46 -0400

    fix date

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import '../assets/events.css';
 
+const events = [
+  { name: 'Bhairav Se Bhairavi Tak', date: '22 JUN 2018', logo: 'https://image.ibb.co/fPdUtJ/0.jpg' },
+  { name: 'Yugpurush', date: '06 AUG 2017', logo: 'https://image.ibb.co/cfejnF/yugpurush_Logo.jpg' },
+  { name: 'Sangam', date: '25 MAY 2017', logo: 'https://image.ibb.co/kAbUt5/Sangam_postcard_5_x_7_1.jpg' },
+  { name: 'Sogaat', date: '22 NOV 2015', logo: 'https://image.ibb.co/nzCxd5/Screen_Shot_2017_04_26_at_10_14_47_PM.png' },
+  { name: 'Shabda Sangat', date: '30 NOV 2014', logo: 'https://image.ibb.co/mEnGo5/Shabda_Sangat.gif' },
+  { name: 'Sur Safar', date: '16 AUG 2014', logo: 'https://image.ibb.co/bFmWMQ/Sur_Safar.png' },
+  { name: 'Ashok Dave', date: '26 APR 2014', logo: 'https://image.ibb.co/dLzAy5/Screen_Shot_2017_04_26_at_10_14_08_PM.png' }
+]
+
 const getEventTile = (eventName, date, posterUrl) => (
-  <div className="col-md-6 col-sm-12 col-xs-12 text-center">
+  <div className="col-lg-6 col-xs-12 text-center">
     <div className="event-card">
       <div className="event-description">
-        <span className="name">{eventName}</span> - {date}
+        <div className="name">{eventName}</div>
+        <div className="event-date">{date}</div>
       </div>
       <div className="text-center">
         <img src={posterUrl} alt="Sangam_postcard_5_x_7_1" className="poster img-rounded" />
@@ -24,7 +35,7 @@ export default function Events() {
           <div className="col-md-10 col-md-offset-1 col-xs-12">
             <div className="date">
               <h3>Srujana & TV Asia Present</h3>
-              <span className="name">Colors of Rajasthan</span> - 22 JUN 2018
+              <span className="name">Colors of Rajasthan</span> - 03 NOV 2018
             </div>
             <div className="text-center">
               <img src="https://image.ibb.co/fRMUv0/COR-with-supporters.jpg" alt="COR-with-supporters" border="0" />
@@ -32,7 +43,6 @@ export default function Events() {
           </div>
         </div>
       </div>
-
       <div className="row heading">
         <div className="col-md-8 col-md-offset-2 col-xs-12">
           <h2>PAST EVENTS</h2><hr className="orange" />
@@ -40,19 +50,7 @@ export default function Events() {
       </div>
       <div className="col-md-8 col-md-offset-2 col-xs-12">
         <div className="row">
-          { getEventTile('Bhairav Se Bhairavi Tak', '22 JUN 2018', 'https://image.ibb.co/fPdUtJ/0.jpg') }
-        {/* </div>
-        <div className="row"> */}
-          { getEventTile('Yugpurush', '06 AUG 2017', 'https://image.ibb.co/cfejnF/yugpurush_Logo.jpg') }
-          { getEventTile('Sangam', '25 MAY 2017', 'https://image.ibb.co/kAbUt5/Sangam_postcard_5_x_7_1.jpg') }
-        {/* </div>
-        <div className="row"> */}
-          { getEventTile('Sogaat', '22 NOV 2015', 'https://image.ibb.co/nzCxd5/Screen_Shot_2017_04_26_at_10_14_47_PM.png') }
-          { getEventTile('Shabda Sangat', '30 NOV 2014', 'https://image.ibb.co/mEnGo5/Shabda_Sangat.gif') }
-        {/* </div>
-        <div className="row"> */}
-          { getEventTile('Sur Safar', '16 AUG 2014', 'https://image.ibb.co/bFmWMQ/Sur_Safar.png') }
-          { getEventTile('Ashok Dave', '26 APR 2014', 'https://image.ibb.co/dLzAy5/Screen_Shot_2017_04_26_at_10_14_08_PM.png') }
+          { events.map(event => getEventTile(event.name, event.date, event.logo)) }
         </div>
       </div>
     </div>
