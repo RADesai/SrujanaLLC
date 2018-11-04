af86bedbb8d0616032a618b2601b2b81551a0f92
Raj Ashish Desai <rajdesai94@gmail.com>
2018-11-03 22:42:39 -0400

    refactoring

--- src/components/Events.js
+++ src/components/Events.js
@@ -2,16 +2,16 @@ import React from 'react';
 import '../assets/events.css';
 
 const events = [
-  { name: 'Bhairav Se Bhairavi Tak', date: '22 JUN 2018', logo: 'https://image.ibb.co/fPdUtJ/0.jpg' },
-  { name: 'Yugpurush', date: '06 AUG 2017', logo: 'https://image.ibb.co/cfejnF/yugpurush_Logo.jpg' },
-  { name: 'Sangam', date: '25 MAY 2017', logo: 'https://image.ibb.co/kAbUt5/Sangam_postcard_5_x_7_1.jpg' },
-  { name: 'Sogaat', date: '22 NOV 2015', logo: 'https://image.ibb.co/nzCxd5/Screen_Shot_2017_04_26_at_10_14_47_PM.png' },
-  { name: 'Shabda Sangat', date: '30 NOV 2014', logo: 'https://image.ibb.co/mEnGo5/Shabda_Sangat.gif' },
-  { name: 'Sur Safar', date: '16 AUG 2014', logo: 'https://image.ibb.co/bFmWMQ/Sur_Safar.png' },
-  { name: 'Ashok Dave', date: '26 APR 2014', logo: 'https://image.ibb.co/dLzAy5/Screen_Shot_2017_04_26_at_10_14_08_PM.png' }
+  { eventName: 'Bhairav Se Bhairavi Tak', date: '22 JUN 2018', posterUrl: 'https://image.ibb.co/fPdUtJ/0.jpg' },
+  { eventName: 'Yugpurush', date: '06 AUG 2017', posterUrl: 'https://image.ibb.co/cfejnF/yugpurush_Logo.jpg' },
+  { eventName: 'Sangam', date: '25 MAY 2017', posterUrl: 'https://image.ibb.co/kAbUt5/Sangam_postcard_5_x_7_1.jpg' },
+  { eventName: 'Sogaat', date: '22 NOV 2015', posterUrl: 'https://image.ibb.co/nzCxd5/Screen_Shot_2017_04_26_at_10_14_47_PM.png' },
+  { eventName: 'Shabda Sangat', date: '30 NOV 2014', posterUrl: 'https://image.ibb.co/mEnGo5/Shabda_Sangat.gif' },
+  { eventName: 'Sur Safar', date: '16 AUG 2014', posterUrl: 'https://image.ibb.co/bFmWMQ/Sur_Safar.png' },
+  { eventName: 'Ashok Dave', date: '26 APR 2014', posterUrl: 'https://image.ibb.co/dLzAy5/Screen_Shot_2017_04_26_at_10_14_08_PM.png' }
 ]
 
-const getEventTile = (eventName, date, posterUrl) => (
+const getEventTile = ({ eventName, date, posterUrl }) => (
   <div className="col-lg-6 col-xs-12 text-center">
     <div className="event-card">
       <div className="event-description">
@@ -50,7 +50,7 @@ export default function Events() {
       </div>
       <div className="col-md-8 col-md-offset-2 col-xs-12">
         <div className="row">
-          { events.map(event => getEventTile(event.name, event.date, event.logo)) }
+          { events.map(event => getEventTile(event)) }
         </div>
       </div>
     </div>
--- src/components/Team.js
+++ src/components/Team.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import '../assets/team.css';
 
-const getTeamMember = (name, phoneNumber) => (
+const teamMembers = [
+  { name: 'Ashish Desai', phoneNumber: '(973) 520-3635' },
+  { name: 'Rathin Mehta', phoneNumber: '(908) 720-9082' },
+  { name: 'Jitendra Shah', phoneNumber: '(609) 510-9746' },
+  { name: 'Jagadish Christian', phoneNumber: '(201) 240-6019' }
+]
+
+const getTeamMember = ({ name, phoneNumber }) => (
   <div className="member">
     <div className="col-md-4 col-md-offset-4 col-xs-12 text-center name">
       <h3>{name}</h3>
@@ -20,10 +27,7 @@ export default function Team() {
         </div>
       </div>
       <div className="row">
-        { getTeamMember('Ashish Desai', '(973) 520-3635') }
-        { getTeamMember('Rathin Mehta', '(908) 720-9082') }
-        { getTeamMember('Jitendra Shah', '(609) 510-9746') }
-        { getTeamMember('Jagadish Christian', '(201) 240-6019') }
+        { teamMembers.map(teamMember => getTeamMember(teamMember)) }
       </div>
     </div>
   );
